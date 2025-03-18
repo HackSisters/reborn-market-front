0face8f000ed99a9603dbb74d3f63be9a6b1c6c0
julia-martynovych <inquam414@gmail.com>
2025-03-18 22:08:23 +0100

    created CardProduct component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import React from 'react'
+import CardProduct from './components/CardProduct'
 
 function App() {
   return (
     <>
       <h1 className="text-red-500"> Hello React + Tailwind </h1>
+      <div className="p-4">
+      <CardProduct
+        image="https://example.com/image.jpg"
+        title="Vestido largo"
+        price="100 $"
+        />
+      </div>
     </>
   )
 }
