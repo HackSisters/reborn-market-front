08a387179b64476da371f8502fd40661e3020444
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-20 10:49:18 +0100

    feat: try call to fetchproducts in home ok status

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../services/FetchService";
+
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function loadData() {
+            try {
+                const res = await fetchProducts();
+                setProducts(res);
+            } catch (err) {
+                console.error('Error fetching media:', err);
+                setError('Error al cargar  los datos. Por favor, intenta  de nuevo.');
+            } 
+
+        }
+        loadData();
+    }, []);
+
+    if(error) return <p>{error}</p>;
+    console.log(products);
+
     return (
         <div>
             <h1>Home</h1>
