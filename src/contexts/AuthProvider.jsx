ae0dd5b33ca7c099c59b28b229ff12f2c6842992
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-20 22:08:51 +0100

    feat:basic login

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,17 +1,31 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { authRequest } from '../services/AuthService';
 const AuthContext = createContext();
 
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isRedirecting, setIsRedirecting] = useState(true);
   const navigate = useNavigate();
 
-  const login = (username, password) => {
+  const login = async (username, password) => {
     if (username && password) {
-      setIsAuthenticated(true); 
+      try {
+        const isAuthenticated = await authRequest(username, password);
+        setIsAuthenticated(isAuthenticated);
+  
+        
+      } catch (error) {
+        console.error('Error:', error);
+      }
     }
   };
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]); 
 
   const logout = () => {
     setIsAuthenticated(false); 
