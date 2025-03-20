c3a6fac0824fe26bd2af9fc522fd68f6694d665a
Israel Espin <israel.espin92@gmail.com>
2025-03-20 18:00:06 +0100

    cookie notice created

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,6 +5,7 @@ import ProductDetail from "../pages/products/ProductDetail";
 import Login from "../pages/users/Login";
 import Contact from "../pages/users/Contact";
 import MainLayout from "../layouts/MainLayout";
+import CookieConsent from "../components/users/CookieConsent";
 
 const AppRouter = () => {
     return (
@@ -16,9 +17,9 @@ const AppRouter = () => {
                     <Route path="/products/:id" element={<ProductDetail />} />
                 </Route>
 
-
                 <Route path="/login" element={<Login />} />
             </Routes>
+            <CookieConsent />
         </AuthProvider>
 
     )
