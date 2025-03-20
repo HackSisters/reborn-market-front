a4669a16b9bfa2e447a17ec4de28774e89842b4a
angiehelensanchez <angiehelensanchez@gmail.com>
2025-03-20 18:29:33 +0100

    fix: import mistake

--- src/components/users/FormAddProducts.jsx
+++ src/components/users/FormAddProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, use } from 'react';
+import React, { useState, useEffect} from 'react';
 import { useForm } from 'react-hook-form';
 import CustomButton from "../ui/buttons/CustomButton";
 
