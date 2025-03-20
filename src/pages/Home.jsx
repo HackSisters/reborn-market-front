fd87c908ce27d19a8b21b810837fb58577fa4d7b
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-20 11:13:23 +0100

    feat: home call refactory to avoid duplicating

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react"
 import { fetchProducts } from "../services/FetchService";
 
 const Home = () => {
+
     const [products, setProducts] = useState([]);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const didFetch = useRef(false);
+
 
     useEffect(() => {
         async function loadData() {
+            
+            if (didFetch.current) return;
+            didFetch.current = true;
             try {
                 const res = await fetchProducts();
                 setProducts(res);
             } catch (err) {
                 console.error('Error fetching media:', err);
                 setError('Error al cargar  los datos. Por favor, intenta  de nuevo.');
-            } 
+            }finally {
+                setLoading(false);
+            }
 
         }
         loadData();
     }, []);
 
     if(error) return <p>{error}</p>;
+    if(loading) return;
     console.log(products);
 
     return (
